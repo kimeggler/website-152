228ff440876c13e5e398118602e27d90118034c0
Kim Eggler <kim.eggler@gmail.com>
2019-11-11 21:03:59 +0100

    finished site

--- src/common/TimelineCard/TimelineCard.jsx
+++ src/common/TimelineCard/TimelineCard.jsx
@@ -1,25 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import { right_arrow, left_arrow } from '../../assets'
-import './_style.css';
+import React, { useState, useEffect } from "react";
+import { right_arrow, left_arrow } from "../../assets";
+import "./_style.css";
 
 function TimelineCard(props) {
   const [style, setStyle] = useState();
 
   useEffect(() => {
-    setStyle({ "align-self": props.position === "left" ? "flex-start" : "flex-end" });
-  }, [props.position])
+    setStyle({
+      "align-self": props.position === "left" ? "flex-start" : "flex-end"
+    });
+  }, [props.position]);
   console.log(style);
 
   return (
     <div className="TimelineCard" style={style}>
       <div className="card-header">
-        <h1>Fancy Header</h1>
-        <h4>12.09.2019</h4>
+        <h2 className="card-title">{props.title}</h2>
+        <h4>{props.author}</h4>
       </div>
       <div className="personal-review">
-        <p>fancy text that describes what we have been doing this week</p>
+        <p>{props.text}</p>
       </div>
-      <img className={props.position === "left" ? "left-arrow" : "right-arrow"} alt="arrow" src={props.position === "left" ? left_arrow : right_arrow} />
+      <img
+        className={props.position === "left" ? "left-arrow" : "right-arrow"}
+        alt="arrow"
+        src={props.position === "left" ? left_arrow : right_arrow}
+      />
     </div>
   );
 }
