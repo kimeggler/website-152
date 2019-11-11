a3e6339ab70e84376a456f001138c50eb718c125
Kim Eggler <kim.eggler@gmail.com>
2019-11-11 19:41:23 +0100

    added edited photos

--- src/views/PhotoSection/PhotoSection.jsx
+++ src/views/PhotoSection/PhotoSection.jsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import ReactCompareImage from 'react-compare-image';
-import ashtray_selim from '../../images/selim/ashtray_unedited.jpg';
-import tippex_selim from '../../images/selim/tippex_unedited.jpg';
-import chair_selim from '../../images/selim/chair_unedited.jpg';
-import stone_tobias from '../../images/tobias/stone_unedited.jpg';
-import tippex_tobias from '../../images/tobias/tippex_unedited.jpg';
-import tower_edited_tobias from '../../images/tobias/tower_edited.jpg';
-import stone_edited_tobias from '../../images/tobias/stone_edited.jpg';
-import tippex_edited_tobias from '../../images/tobias/tippex_edited.jpg';
-import tower_tobias from '../../images/tobias/tower_unedited.jpg';
-import ashtray_kim from '../../images/kim/ashtray_unedited.jpg';
-import bottle_kim from '../../images/kim/bottle_unedited.jpg';
-import lock_kim from '../../images/kim/lock_unedited.jpg';
-import './_style.css';
+import React from "react";
+import ReactCompareImage from "react-compare-image";
+import ashtray_selim from "../../images/selim/ashtray_unedited.jpg";
+import tippex_selim from "../../images/selim/tippex_unedited.jpg";
+import chair_selim from "../../images/selim/chair_unedited.jpg";
+import ashtray_edited_selim from "../../images/selim/ashtray_edited.jpg";
+import tippex_edited_selim from "../../images/selim/tippex_edited.jpg";
+import chair_edited_selim from "../../images/selim/chair_edited.jpg";
+import stone_tobias from "../../images/tobias/stone_unedited.jpg";
+import tippex_tobias from "../../images/tobias/tippex_unedited.jpg";
+import tower_tobias from "../../images/tobias/tower_unedited.jpg";
+import tower_edited_tobias from "../../images/tobias/tower_edited.jpg";
+import stone_edited_tobias from "../../images/tobias/stone_edited.jpg";
+import tippex_edited_tobias from "../../images/tobias/tippex_edited.jpg";
+import ashtray_kim from "../../images/kim/ashtray_unedited.jpg";
+import bottle_kim from "../../images/kim/bottle_unedited.jpg";
+import lock_kim from "../../images/kim/lock_unedited.jpg";
+import ashtray_edited_kim from "../../images/kim/ashtray_edited.jpg";
+import bottle_edited_kim from "../../images/kim/bottle_edited.jpg";
+import lock_edited_kim from "../../images/kim/lock_edited.jpg";
+import "./_style.css";
 
 function PhotoSection() {
-
   return (
     <div className="PhotoSection">
       <h2 className="site-title">PHOTOS</h2>
@@ -23,7 +28,10 @@ function PhotoSection() {
         <h2 className="section-title">PHOTOS FROM SELIM</h2>
         <div className="image-box">
           <h3>"Ashtray"</h3>
-          <ReactCompareImage leftImage={ashtray_selim} rightImage={ashtray_selim} />
+          <ReactCompareImage
+            leftImage={ashtray_selim}
+            rightImage={ashtray_edited_selim}
+          />
           <div className="image-description">
             <div className="description-group">
               <h4 className="description-title">Exposure</h4>
@@ -41,7 +49,10 @@ function PhotoSection() {
         </div>
         <div className="image-box">
           <h3>"Tipp-Ex"</h3>
-          <ReactCompareImage leftImage={tippex_selim} rightImage={tippex_selim} />
+          <ReactCompareImage
+            leftImage={tippex_selim}
+            rightImage={tippex_edited_selim}
+          />
           <div className="image-description">
             <div className="description-group">
               <h4 className="description-title">Exposure</h4>
@@ -59,7 +70,10 @@ function PhotoSection() {
         </div>
         <div className="image-box">
           <h3>"Chair"</h3>
-          <ReactCompareImage leftImage={chair_selim} rightImage={chair_selim} />
+          <ReactCompareImage
+            leftImage={chair_selim}
+            rightImage={chair_edited_selim}
+          />
           <div className="image-description">
             <div className="description-group">
               <h4 className="description-title">Exposure</h4>
@@ -76,12 +90,15 @@ function PhotoSection() {
           </div>
         </div>
         <div className="section-splitter" />
-      </section >
+      </section>
       <section className="tobias image-section">
         <h2 className="section-title">PHOTOS FROM TOBIAS</h2>
         <div className="image-box">
           <h3>"Stone"</h3>
-          <ReactCompareImage leftImage={stone_tobias} rightImage={stone_edited_tobias} />
+          <ReactCompareImage
+            leftImage={stone_tobias}
+            rightImage={stone_edited_tobias}
+          />
           <div className="image-description">
             <div className="description-group">
               <h4 className="description-title">Exposure</h4>
@@ -99,7 +116,10 @@ function PhotoSection() {
         </div>
         <div className="image-box">
           <h3>"Tipp-Ex"</h3>
-          <ReactCompareImage leftImage={tippex_tobias} rightImage={tippex_edited_tobias} />
+          <ReactCompareImage
+            leftImage={tippex_tobias}
+            rightImage={tippex_edited_tobias}
+          />
           <div className="image-description">
             <div className="description-group">
               <h4 className="description-title">Exposure</h4>
@@ -117,7 +137,10 @@ function PhotoSection() {
         </div>
         <div className="image-box">
           <h3>"Tower"</h3>
-          <ReactCompareImage leftImage={tower_tobias} rightImage={tower_edited_tobias} />
+          <ReactCompareImage
+            leftImage={tower_tobias}
+            rightImage={tower_edited_tobias}
+          />
           <div className="image-description">
             <div className="description-group">
               <h4 className="description-title">Exposure</h4>
@@ -139,7 +162,10 @@ function PhotoSection() {
         <h2 className="section-title">PHOTOS FROM KIM</h2>
         <div className="image-box">
           <h3>"Ashtray"</h3>
-          <ReactCompareImage leftImage={ashtray_kim} rightImage={ashtray_kim} />
+          <ReactCompareImage
+            leftImage={ashtray_kim}
+            rightImage={ashtray_edited_kim}
+          />
           <div className="image-description">
             <div className="description-group">
               <h4 className="description-title">Exposure</h4>
@@ -157,7 +183,10 @@ function PhotoSection() {
         </div>
         <div className="image-box">
           <h3>"Bottle"</h3>
-          <ReactCompareImage leftImage={bottle_kim} rightImage={bottle_kim} />
+          <ReactCompareImage
+            leftImage={bottle_kim}
+            rightImage={bottle_edited_kim}
+          />
           <div className="image-description">
             <div className="description-group">
               <h4 className="description-title">Exposure</h4>
@@ -175,7 +204,10 @@ function PhotoSection() {
         </div>
         <div className="image-box">
           <h3>"Lock"</h3>
-          <ReactCompareImage leftImage={lock_kim} rightImage={lock_kim} />
+          <ReactCompareImage
+            leftImage={lock_kim}
+            rightImage={lock_edited_kim}
+          />
           <div className="image-description">
             <div className="description-group">
               <h4 className="description-title">Exposure</h4>
@@ -193,7 +225,7 @@ function PhotoSection() {
         </div>
         <div className="section-splitter" />
       </section>
-    </div >
+    </div>
   );
 }
 
